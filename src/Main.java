2a75a7825e50eab01032219714b38c9f76b4ea42
pqd <852020471@qq.com>
2022-08-28 20:37:07 +0800

    更改bug

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,8 @@
  * @Author:852020471@qq.com
  */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        System.out.println("Test");
     }
 }
